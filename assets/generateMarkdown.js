f404b9fdb8d7ae1d06318f6573adac3e45e4b5cf
dMosca <devinmosca@yahoo.com>
2021-08-25 20:33:36 -0600

    Working on generateMarkdown.js

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,14 +1,23 @@
+// ["Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 2-clause Simplified license", "BSD 3-clause New or Revised license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The F*ck You Want To Public License", "Eclipse Public License 1.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0", "GNU Lesser General Public License family", "GNU Lesser General Public License v3.0", "ISC", "MIT", "Mozilla Public License 2.0", "SIL Open Font License 1.1", "The Unlicense", "zLib License"]
+
 // // TODO: Create a function that returns a license badge based on which license is passed in
 // // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge = "";
+  switch(license) {
+    case "Apache license 2.0":
+      licenseBadge = "https://img.shields.io/badge/License-Apache%202.0-blue.svg"
+      break;
+  }
+}
 
 // // TODO: Create a function that returns the license link
 // // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+function renderLicenseLink(license) {}
 
 // // TODO: Create a function that returns the license section of README
 // // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
+function renderLicenseSection(license) {}
 
 // // TODO: Create a function to generate markdown for README
 // function generateMarkdown(data) {
@@ -17,8 +26,12 @@
 // `;
 // }
 
+
+
 module.exports = generateMarkdown;
 
 const generateMarkdown = (answers) =>
-
-``;
+  let 
+  return `
+  
+  `
